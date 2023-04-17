a2f8de43b4ac39d4d1c5cfdc8b1376dd8cf56988
Marcelo Gonçalves de Souza <mgoncalves@digitalhouse.com>
2023-04-17 19:44:42 -0300

    Update

--- 24-DesignPatterns/Factory2/Banco/src/main/java/org/example/Main.java
+++ 24-DesignPatterns/Factory2/Banco/src/main/java/org/example/Main.java
@@ -1,16 +1,18 @@
 package org.example;
 
 import org.example.entities.Banco;
+import org.example.entities.BoletoSimpleFactory;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        Banco banco = new Banco();
+        BoletoSimpleFactory boletoSimpleFactory = new BoletoSimpleFactory();
+
+        Banco banco = new Banco(boletoSimpleFactory);
         banco.gerarBoleto(10, 1000.0);
         banco.gerarBoleto(30, 1000.0);
         banco.gerarBoleto(60, 1000.0);
         banco.gerarBoleto(90, 1000.0);
-
     }
 
 }
